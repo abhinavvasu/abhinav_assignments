48ad18ee4cab01d9755366be755d37fe3bb1dfbf
abhinavvasu <abhinav.vasu3@gmail.com>
2019-09-19 13:16:17 -0400

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,25 @@
+print("Abhinav Vasu")
+
+# Precedence is PEMDAS
+a = 10 / 3
+b = 10 // 3
+c = 5 / 2
+d = 2**2
+print("a is equal to", a)
+print("b is equal to", b)
+print("c is equal to", c)
+print("d is equal to", d)
+
+# Logical operators are used to combine conditional statements
+x = 5
+print(x > 3 and x < 10)
+print(x > 3 or x < 4)
+print(not(x > 3 and x < 10))
+
+# Exponents
+x = 2
+print(x ** 2)
+
+# Exponents
+x = 5
+print(x % 2)

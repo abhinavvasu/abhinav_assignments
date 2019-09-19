5877209562a073667086ea2362af78232515fd50
abhinavvasu <abhinav.vasu3@gmail.com>
2019-09-19 13:10:36 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,10 @@
+planet="Jupiter"
+circumference=40075
+amount=1.234
+student=True
+
+print("Yout First and Last Name")
+print(type(planet))
+print(type(circumference))
+print(type(amount))
+print(type(student))
